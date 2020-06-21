05cdc9f275dd88bec0d595161be48c9ce423e736
Takuya-Mizutani <mtty1017@gmail.com>
2020-06-22 08:48:42 +0900

    done

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -13,6 +13,7 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    print(post)
     return render(request, 'blog/post_detail.html', {'post':post})
 
 
